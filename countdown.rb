1d0ce009251f6b3c13a0ce31239be6e8fea12a1a
chrstno040420 <104917222+chrstno040420@users.noreply.github.com>
2022-07-28 16:06:21 -0500

    commit tree b87b00584ab0ea1addfd4bc8afa18371c09b0da8

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
+require "pry"
+def countdown(number)
+    
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1 
+    end
 
-def countdown
+"HAPPY NEW YEAR!"
+
 end
+
+def countdown_with_sleep(countdown)
+   
+  sleep 5
+
+end
+
+# 2. Our Ruby program executes so quickly that it doesn't actually count down at
+#    the speed of one second per number. See if you can make the loop pause for
+#    one second each trip around ([hint][sleep hint]).
+
+#    Write this in a new method called `countdown_with_sleep` that also takes one
+#    integer argument for the countdown.
